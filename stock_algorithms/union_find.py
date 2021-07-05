04ffe9e177a32ca37781db1f128f4f68b391cab9
Hiroo Kusaba <hrksb5029@gmail.com>
2021-07-06 00:07:22 +0900

    Union Findのclassを作成する

--- stock_algorithms/union_find.py
+++ stock_algorithms/union_find.py
@@ -1,32 +1,44 @@
 # https://atcoder.jp/contests/atc001/tasks/unionfind_a
+
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.parents = [i for i in range(n + 1)]
+        self.size = [1] * (n + 1)
+
+    def root(self, x: int) -> int:
+        if self.parents[x] == x:
+            return x
+        self.parents[x] = self.root(self.parents[x])
+        return self.parents[x]
+
+    def is_same_group(self, x: int, y: int) -> bool:
+        return self.root(x) == self.root(y)
+
+    def get_group_size(self, x: int) -> int:
+        return self.size[self.root(x)]
+
+    def unite(self, x: int, y: int):
+        gx = self.root(x)
+        gy = self.root(y)
+        if gx == gy:
+            return 0
+        if self.size[gx] < self.size[gy]:
+            self.parents[gx] = gy
+            self.size[gy] += self.size[gx]
+        else:
+            self.parents[gy] = gx
+            self.size[gx] += self.size[gy]
+
+
 N, Q = map(int, input().split())
 par = [i for i in range(N + 1)]
-
-
-def root(x: int) -> int:
-    if par[x] == x:
-        return x
-    par[x] = root(par[x])
-    return par[x]
-
-
-def is_same_root(x: int, y: int) -> bool:
-    return root(x) == root(y)
-
-
-def unite(x: int, y: int):
-    x = root(x)
-    y = root(y)
-    if x == y:
-        return 0
-    par[x] = y
-
+uf = UnionFind(N + 1)
 
 for _ in range(Q):
     p, a, b = map(int, input().split())
     if p == 0:
-        unite(a, b)
+        uf.unite(a, b)
     else:
-        ans = 'Yes' if is_same_root(a, b) else 'No'
+        ans = 'Yes' if uf.is_same_group(a, b) else 'No'
         print(ans)
-
