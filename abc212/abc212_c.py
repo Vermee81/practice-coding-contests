4f2d14dc9b1a7d457c687f4889e2350281a1bc7e
Hiroo Kusaba <hrksb5029@gmail.com>
2021-08-04 02:20:46 +0900

    ABC212のC問題を修正する

--- abc212/abc212_c.py
+++ abc212/abc212_c.py
@@ -1,27 +1,18 @@
 # https://atcoder.jp/contests/abc212/tasks/abc212_c
-from bisect import bisect_left
 N, M = map(int, input().split())
 a_arr = list(map(int, input().split()))
 a_arr.sort()
 b_arr = list(map(int, input().split()))
 b_arr.sort()
-if a_arr[N - 1] < b_arr[0]:
-    print(abs(a_arr[N - 1] - b_arr[0]))
-    exit()
-if b_arr[N - 1] < a_arr[0]:
-    print(abs(b_arr[M - 1] - a_arr[0]))
-    exit()
 
 ans = 10**10
-start_j = 0
-for i in range(N):
-    start_j = bisect_left(a_arr, a_arr[i], )
-    before_diff = 10**10
-    for j in range(start_j, M):
-        diff = abs(a_arr[i] - b_arr[j])
-        if before_diff < diff:
-            start_j = j
-            break
-        ans = min(ans, diff)
+i = 0
+j = 0
+while i < N and j < M:
+    ans = min(ans, abs(a_arr[i] - b_arr[j]))
+    if a_arr[i] > b_arr[j]:
+        j += 1
+    else:
+        i += 1
 
 print(ans)
